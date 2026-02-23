28aae295aadbae0fcb37d1095b3db7b8800b9d3a
717atom <thesmallestparticleever@gmail.com>
2026-02-23 13:12:52 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+console.log(`Hello from js`)
+let today = new Date().toLocaleDateString();
+
+let saved = localStorage.getItem(today);
+
+let track = {
+    weight: 0,
+    food: []
+};
+
+track.food.push({name: "oatmeal",calories: 150, protien: 5});
+
+let totalCalories = 0;
+
+for (let i = 0; i < track.food.length; i++) {
+    totalCalories += track.food[i].calories;
+}
+
+let track_cerealized = JSON.stringify(track);
+
+localStorage.setItem(today,track_cerealized);
+
+document.getElementById("date").textContent = today;
+document.getElementById('weight').textContent = track.weight;
+
+
+
+console.log(saved);
+
+
+
+
+
+
+
+
+
+/* will need this later
+window popups
+window.alert(`boo`)
+
+local storage
+localStorage.setItem('The Name',input.value);
+le track_cerealized = json.stringify(track)
+*/
